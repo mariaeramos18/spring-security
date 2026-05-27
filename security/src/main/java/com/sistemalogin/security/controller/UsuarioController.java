21c9e5e13be231aae235407d3c261b73abebaede
Maria <mariaeramosfacul@gmail.com>
2026-05-27 20:07:50 -0300

    add get e post dos arquivos .java e index.html

--- security/src/main/java/com/sistemalogin/security/controller/UsuarioController.java
+++ security/src/main/java/com/sistemalogin/security/controller/UsuarioController.java
@@ -2,7 +2,18 @@ package com.sistemalogin.security.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.sistemalogin.security.model.Usuario;
 import com.sistemalogin.security.model.UsuarioRepository;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @Controller
 public class UsuarioController {
@@ -11,7 +22,30 @@ public class UsuarioController {
     private UsuarioRepository usuarioRepository;
 
     //GET -> Página index
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    
     //GET -> Página de cadastro
+    @GetMapping("/cadastro")// localhost:8080/cadastro
+    public String cadastroUsario(Model model){
+        model.addAttribute("usuario", new Usuario());
+        return "cadastro";
+    }
+
     //GET -> Página de logada
+    @PostMapping("/cadastro")
+    public String cadstroRealizado(Model model) {        
+        return "redirect:/";
+    }
+    
+    @GetMapping("/logado")
+        public String areaLogado(){
+            return "logado";
+        }
+
     //POST -> Cadastro de usuário
+
+
 }
--- security/src/main/java/com/sistemalogin/security/services/UserDetailsService.java
+++ security/src/main/java/com/sistemalogin/security/services/UserDetailsService.java
@@ -1,8 +1,19 @@
 package com.sistemalogin.security.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sistemalogin.security.model.UsuarioRepository;
+
 @Service
-public class UserDetailsService {
+public class UserDetailsService implements UserDetailsService{
     
+    // Injeção para acessar os dados do usuario no banco de dados para autenticação
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    //Metodo que sera chamado SS para carregar os detalhes do usuário com base no nome do usuário(neste caso, será o e-mail) fornecido durante o processo de autenticação
+    public UserDetails carregarDadosPorEmail(String email){
+        
+    }
 }
